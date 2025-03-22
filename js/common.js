679c40c44875fb8682f3ad67fcf4d16b9ed97beb
Geoffrey Sperl <geoffrey@geoffreysperl.com>
2025-03-22 10:57:59 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -7,114 +7,28 @@ document.addEventListener("DOMContentLoaded", function() {
     toggleTheme = document.querySelector(".toggle-theme-js"),
     btnScrollToTop = document.querySelector(".top");
 
-
-  /* =======================================================
-  // Menu + Theme Switcher
-  ======================================================= */
+  // Menu Toggle
   menuToggle.addEventListener("click", () => {
-    menu();
-  });
-
-  function menuOpen() {
-    menuList.classList.add("is-open");
-  }
-
-
-  // Menu
-  function menu() {
     menuToggle.classList.toggle("is-open");
     menuList.classList.toggle("is-visible");
-  }
-
-  if (toggleTheme) {
-    toggleTheme.addEventListener("click", () => {
-      darkMode();
-    });
-  };
-
+  });
 
   // Theme Switcher
-  function darkMode() {
-    if (html.classList.contains('dark-mode')) {
-      html.classList.remove('dark-mode');
-      localStorage.removeItem("theme");
-      document.documentElement.removeAttribute("dark");
-    } else {
-      html.classList.add('dark-mode');
-      localStorage.setItem("theme", "dark");
-      document.documentElement.setAttribute("dark", "");
-    }
-  }
-
-
-  /* ================================================================
-  // Stop Animations During Window Resizing and Switching Theme Modes
-  ================================================================ */
-  let disableTransition;
-
   if (toggleTheme) {
     toggleTheme.addEventListener("click", () => {
-      stopAnimation();
-    });
-
-    window.addEventListener("resize", () => {
-      stopAnimation();
-    });
-
-    function stopAnimation() {
-      document.body.classList.add("disable-animation");
-      clearTimeout(disableTransition);
-      disableTransition = setTimeout(() => {
-        document.body.classList.remove("disable-animation");
-      }, 100);
-    }
-  }
-
-
-  /* =======================
-  // Responsive Videos
-  ======================= */
-  reframe(".post iframe:not(.reframe-off), .page iframe:not(.reframe-off)");
-
-
-  /* =======================
-  // LazyLoad Images
-  ======================= */
-  var lazyLoadInstance = new LazyLoad({
-    elements_selector: ".lazy"
-  })
-
-
-  /* =======================
-  // Zoom Image
-  ======================= */
-  const lightense = document.querySelector(".page img, .post img, .gallery__image img"),
-  imageLink = document.querySelectorAll(".page a img, .post a img, .gallery__image a img");
-
-  if (imageLink) {
-    for (var i = 0; i < imageLink.length; i++) imageLink[i].parentNode.classList.add("image-link");
-    for (var i = 0; i < imageLink.length; i++) imageLink[i].classList.add("no-lightense");
-  }
-
-  if (lightense) {
-    Lightense(".page img:not(.no-lightense), .post img:not(.no-lightense), .gallery__image img:not(.no-lightense)", {
-    padding: 60,
-    offset: 30
+      if (html.classList.contains('dark-mode')) {
+        html.classList.remove('dark-mode');
+        localStorage.removeItem("theme");
+        document.documentElement.removeAttribute("dark");
+      } else {
+        html.classList.add('dark-mode');
+        localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("dark", "");
+      }
     });
   }
 
-
-  // =====================
-  // Load More Posts
-  // =====================
-  var load_posts_button = document.querySelector('.load-more-posts');
-
-  load_posts_button&&load_posts_button.addEventListener("click",function(e){e.preventDefault();var o=document.querySelector(".pagination"),e=pagination_next_url.split("/page")[0]+"/page/"+pagination_next_page_number+"/";fetch(e).then(function(e){if(e.ok)return e.text()}).then(function(e){var n=document.createElement("div");n.innerHTML=e;for(var t=document.querySelector(".grid"),a=n.querySelectorAll(".grid__post"),i=0;i<a.length;i++)t.appendChild(a.item(i));new LazyLoad({elements_selector:".lazy"});pagination_next_page_number++,pagination_next_page_number>pagination_available_pages_number&&(o.style.display="none")})});
-
-
-  /* =======================
   // Scroll Top Button
-  ======================= */
   window.addEventListener("scroll", function () {
     window.scrollY > window.innerHeight ? btnScrollToTop.classList.add("is-active") : btnScrollToTop.classList.remove("is-active");
   });
@@ -125,8 +39,7 @@ document.addEventListener("DOMContentLoaded", function() {
         top: 0,
         left: 0,
         behavior: "smooth"
-      })
+      });
     }
   });
-
 });
